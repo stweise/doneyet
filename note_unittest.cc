093b4750282606e9a620545206eaa66ce0ddb8d0
Steffen Weise <steffen.weise@mercateo.com>
2019-06-08 09:02:06 +0200

    changing testing macro

--- note_unittest.cc
+++ note_unittest.cc
@@ -3,19 +3,29 @@
 #include "serializer.h"
 
 TEST(NoteTest, OneIsOne) {
-  EXPECT_EQ(1, 1);
+  ASSERT_EQ(1, 1);
 }
 
 TEST(NoteTest, CreateFromStringAndDestroy) {
   string name = "foo";
   Note* note = new Note(name);
-  EXPECT_STREQ(name.c_str(), note->GetText().c_str());
+  ASSERT_NE(note, nullptr);
+  ASSERT_STREQ(name.c_str(), note->GetText().c_str());
   delete note;
 }
-	
+
+TEST(NoteTest, CreateFromEmptyStringAndDestroy) {
+  string name = "";
+  Note* note = new Note(name);
+  ASSERT_NE(note, nullptr);
+  ASSERT_STREQ(name.c_str(), note->GetText().c_str());
+  delete note;
+}
+
 TEST(NoteTest, SerializedAndUnSerializedNoteMatchInText) {
   string name = "foo2";
   Note* note = new Note(name);
+  ASSERT_NE(note, nullptr);
   // serialize it
   string serializedFilename = "/tmp/NoteTest.project";
   Serializer*  s = new Serializer("", serializedFilename);
@@ -25,19 +35,21 @@ TEST(NoteTest, SerializedAndUnSerializedNoteMatchInText) {
   // clear current instance of note
   delete note;
   note = new Note("otherfoo");
+  ASSERT_NE(note, nullptr);
 
   // unserialze it
   Serializer* s2 = new Serializer(serializedFilename, "");
   note->ReadFromSerializer(s2);
   delete s2;
 
-  EXPECT_STREQ(name.c_str(), note->GetText().c_str());
+  ASSERT_STREQ(name.c_str(), note->GetText().c_str());
   delete note;
 }
 
 TEST(NoteTest, SerializedAndUnSerializedNoteMatchInTextWithDate) {
   string name = "foo2";
   Note* note = new Note(name);
+  ASSERT_NE(note, nullptr);
 
   // serialize it
   string serializedFilename = "/tmp/NoteTest.project";
@@ -49,12 +61,13 @@ TEST(NoteTest, SerializedAndUnSerializedNoteMatchInTextWithDate) {
   // clear current instance of note
   delete note;
   note = new Note("otherfoo");
+  ASSERT_NE(note, nullptr);
 
   // unserialze it
   Serializer* s2 = new Serializer(serializedFilename, "");
   note->ReadFromSerializer(s2);
   delete s2;
 
-  EXPECT_STREQ(textWithDate.c_str(), note->Text().c_str());
+  ASSERT_STREQ(textWithDate.c_str(), note->Text().c_str());
   delete note;
 }
